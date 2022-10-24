202ddfc9e26384ac741ad8169dca57895daef665
htrepo <himanshutecha@gmail.com>
2022-10-24 15:08:19 +0530

    oauth2 set up uising github

--- src/main/java/com/ht/springsecurity/controller/Hello.java
+++ src/main/java/com/ht/springsecurity/controller/Hello.java
@@ -1,17 +1,22 @@
 package com.ht.springsecurity.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
 
     @GetMapping
-    public String hello(Principal principal){
-        return "Hi "+principal.getName();
+    public String get(Principal principal){
+        OAuth2User principal1 = ((OAuth2AuthenticationToken) principal).getPrincipal();
+        Object login = principal1.getAttribute("login");
+        return "hi "+ login +" authorities:"+principal1.getAuthorities().stream().collect(Collectors.toList());
     }
 }
--- src/main/java/com/ht/springsecurity/data/repo/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ht.springsecurity.data.repo;
-
-import com.ht.springsecurity.data.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
